fa1278f774f1c3fd5e685344a847784b679a10c2
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-06-02 15:41:23 -0400

    Add track table

--- app/src/main/java/com/cliffracertech/soundaura/model/database/Playlist.kt
+++ app/src/main/java/com/cliffracertech/soundaura/model/database/Playlist.kt
@@ -6,22 +6,54 @@ package com.cliffracertech.soundaura.model.database
 import android.net.Uri
 import androidx.annotation.FloatRange
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
 import androidx.room.*
 import com.cliffracertech.soundaura.R
+import com.cliffracertech.soundaura.collectAsState
+import com.cliffracertech.soundaura.containsBlanks
+import com.cliffracertech.soundaura.model.ListValidator
+import com.cliffracertech.soundaura.model.StringResource
+import com.cliffracertech.soundaura.model.Validator
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import java.io.File
+
+@Entity(tableName = "track")
+data class Track(@PrimaryKey val uri: Uri) {
+    class UriConverter {
+        @TypeConverter fun fromString(string: String) = string.toUri()
+        @TypeConverter fun toString(uri: Uri) = uri.toString()
+    }
+}
+
+@Entity(tableName = "playlistTrack",
+        primaryKeys = ["playlistName", "trackUri"],
+    foreignKeys = [
+        ForeignKey(entity = Playlist::class,
+            parentColumns=["name"],
+            childColumns=["playlistName"],
+            onUpdate=ForeignKey.CASCADE,
+            onDelete=ForeignKey.CASCADE),
+        ForeignKey(entity = Track::class,
+            parentColumns=["uri"],
+            childColumns=["trackUri"],
+            onUpdate=ForeignKey.CASCADE,
+            onDelete=ForeignKey.CASCADE)])
+data class PlaylistTrack(
+    val playlistName: String,
+    val trackUri: Uri,
+    val playlistOrder: Int)
 
 @Entity(tableName = "playlist")
 data class Playlist(
     /** The name of the [Playlist]. */
     @PrimaryKey
     val name: String,
-
-    /** A [List] of the [Uri]s of the tracks in the [Playlist]. */
-    val tracks: List<Uri>,
 
     /** Whether or not shuffle is enabled for the [Playlist]. */
     @ColumnInfo(defaultValue = "0")
@@ -36,8 +68,6 @@ data class Playlist(
     @ColumnInfo(defaultValue = "1.0")
     val volume: Float = 1f,
 ) {
-    val hasError get() = tracks.isEmpty()
-
     class TracksConverter {
         @TypeConverter fun fromString(string: String) =
             string.split(File.pathSeparatorChar).map(String::toUri)
@@ -57,12 +87,32 @@ data class Playlist(
 }
 
 @Dao abstract class PlaylistDao {
-    /** Insert the [playlist] into the database. */
-    @Insert abstract suspend fun insert(playlist: Playlist): Long
-
     /** Insert the [Playlist]s in [playlists] into the database. */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    abstract suspend fun insert(playlists: List<Playlist>): List<Long>
+    protected abstract suspend fun insertPlaylists(playlists: List<Playlist>)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    protected abstract suspend fun insertTracks(tracks: List<Uri>): List<Uri>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    protected abstract suspend fun insertPlaylistContents(playlistName: String, tracks: List<Uri>)
+
+    @Query("DELETE FROM playlistTrack WHERE playlistName = :playlistName")
+    protected abstract suspend fun deletePlaylistContents(playlistNames: List<String>)
+
+    @Query("INSERT INTO playlistTrack SELECT :playlistName, uri, row_number() FROM (:uris)")
+    protected abstract suspend fun addPlaylistContents(playlistName: String, uris: List<Uri>)
+
+    @Transaction
+    suspend fun insert(playlistContentMap: Map<Playlist, List<Uri>>) {
+        val playlists = playlistContentMap.keys.toList()
+        insertPlaylists(playlists)
+        deletePlaylistContents(playlists.map(Playlist::name))
+        for (playlist in playlists) {
+
+            addPlaylistContents(playlist.name, playlistContentMap[playlist] ?: emptyList())
+        }
+    }
 
     /** Delete the playlist whose name matches [name] from the database. */
     @Query("DELETE FROM playlist WHERE name = :name")
@@ -71,6 +121,10 @@ data class Playlist(
     /** Delete the [Playlist]s whose names are in [names] from the database. */
     @Query("DELETE FROM playlist WHERE name in (:names)")
     abstract suspend fun delete(names: List<String>)
+
+    /** Return whether or not a [Playlist] whose name matches [name] exists. */
+    @Query("SELECT EXISTS(SELECT name FROM playlist WHERE name = :name)")
+    abstract suspend fun exists(name: String?): Boolean
 
     @Query("SELECT * FROM playlist WHERE name LIKE :filter ORDER BY name COLLATE NOCASE ASC")
     protected abstract fun getAllPlaylistsSortedByNameAsc(filter: String): Flow<List<Playlist>>
@@ -153,4 +207,48 @@ data class Playlist(
     /** Set the [Playlist.volume] field of the [Playlist] identified by [name]. */
     @Query("UPDATE playlist SET volume = :volume WHERE name = :name")
     abstract suspend fun setVolume(name: String, volume: Float)
+}
+
+class TrackNamesValidator(
+    private val playlistDao: PlaylistDao,
+    coroutineScope: CoroutineScope,
+    names: List<String>,
+) : ListValidator<String>(coroutineScope, names) {
+
+    private val existingNames by playlistDao
+        .getPlaylistNames()
+        .map(List<String>::toSet)
+        .collectAsState(emptySet(), coroutineScope)
+
+    override fun isValid(value: String) = value !in existingNames || value.isBlank()
+
+    override suspend fun messageFor(values: List<Pair<String, Boolean>>) =
+        if (values.find { it.second } == null) null
+        else Validator.Message.Error(
+            StringResource(
+            R.string.add_multiple_tracks_name_error_message)
+        )
+
+    override suspend fun validate(): List<String>? {
+        val existingNames = playlistDao.getPlaylistNames().first().toSet()
+        val names = values.map { it.first }
+        return when {
+            names.intersect(existingNames).isNotEmpty() -> null
+            names.containsBlanks() -> null
+            else -> names
+        }
+    }
+}
+
+class PlaylistNameValidator(
+    private val playlistDao: PlaylistDao,
+    initialName: String
+) : Validator<String>(initialName) {
+    override suspend fun messageFor(value: String) = when {
+        value.isBlank() ->
+            Message.Error(StringResource(R.string.add_playlist_blank_name_error_message))
+        playlistDao.exists(value) ->
+            Message.Error(StringResource(R.string.add_playlist_duplicate_name_error_message))
+        else -> null
+    }
 }
