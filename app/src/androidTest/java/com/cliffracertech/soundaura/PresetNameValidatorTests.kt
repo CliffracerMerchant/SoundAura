8adf3c71671d763650376e5e34c1ef69be02c2e4
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-08-21 19:58:46 -0400

    Add PresetNameValidatorTests

--- app/src/androidTest/java/com/cliffracertech/soundaura/PresetNameValidatorTests.kt
+++ app/src/androidTest/java/com/cliffracertech/soundaura/PresetNameValidatorTests.kt
@@ -9,10 +9,10 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.cliffracertech.soundaura.model.Validator
-import com.cliffracertech.soundaura.model.database.PlaylistDao
+import com.cliffracertech.soundaura.model.database.PresetDao
 import com.cliffracertech.soundaura.model.database.SoundAuraDatabase
-import com.cliffracertech.soundaura.model.database.newPlaylistNameValidator
-import com.cliffracertech.soundaura.model.database.playlistRenameValidator
+import com.cliffracertech.soundaura.model.database.newPresetNameValidator
+import com.cliffracertech.soundaura.model.database.presetRenameValidator
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.runBlocking
@@ -23,25 +23,29 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class PlaylistNameValidatorTests {
+class PresetNameValidatorTests {
     private val context = ApplicationProvider.getApplicationContext<Context>()
     private val coroutineScope = TestCoroutineScope()
 
     private lateinit var instance: Validator<String>
     private lateinit var db: SoundAuraDatabase
-    private lateinit var playlistDao: PlaylistDao
+    private lateinit var presetDao: PresetDao
 
-    private val existingName1 = "playlist 1"
-    private val existingName2 = "playlist 2"
-    private val newPlaylistName = "new playlist"
+    private val existingName = "preset 1"
+    private val existingName2 = "preset 2"
+    private val newPresetName = "new preset"
 
     @Before fun init() {
         db = Room.inMemoryDatabaseBuilder(context, SoundAuraDatabase::class.java).build()
-        playlistDao = db.playlistDao()
+        presetDao = db.presetDao()
         runBlocking {
-            playlistDao.insertSingleTrackPlaylists(
-                playlistNames = listOf(existingName1, existingName2),
-                trackUris = listOf("test uri 1", "test uri 2").map(String::toUri))
+            val names = listOf("track 1")
+            db.playlistDao().insertSingleTrackPlaylists(
+                playlistNames = names,
+                trackUris = listOf("test uri".toUri()))
+            db.playlistDao().toggleIsActive(names.first())
+            presetDao.savePreset(existingName)
+            presetDao.savePreset(existingName2)
         }
     }
 
@@ -51,7 +55,7 @@ class PlaylistNameValidatorTests {
     }
 
     private fun initNewNameValidator() {
-        instance = newPlaylistNameValidator(playlistDao, coroutineScope)
+        instance = newPresetNameValidator(presetDao, coroutineScope)
     }
 
     @Test fun new_name_validator_begins_blank_without_error() = runTest{
@@ -63,7 +67,7 @@ class PlaylistNameValidatorTests {
 
     @Test fun new_name_validator_shows_error_for_existing_name() = runTest {
         initNewNameValidator()
-        instance.value = existingName1
+        instance.value = existingName
         waitUntil { instance.message != null }
         assertThat(instance.message).isInstanceOf(Validator.Message.Error::class)
         assertThat(instance.message?.stringResource?.stringResId)
@@ -72,7 +76,7 @@ class PlaylistNameValidatorTests {
 
     @Test fun new_name_validator_shows_error_for_blank_name_after_change() = runTest {
         initNewNameValidator()
-        instance.value = newPlaylistName
+        instance.value = newPresetName
         instance.value = ""
         waitUntil { instance.message != null }
         assertThat(instance.message).isInstanceOf(Validator.Message.Error::class)
@@ -92,7 +96,7 @@ class PlaylistNameValidatorTests {
 
     @Test fun new_name_validator_fails_validation_for_existing_name() = runTest {
         initNewNameValidator()
-        instance.value = existingName1
+        instance.value = existingName
         val result = instance.validate()
         waitUntil {
             instance.message?.stringResource?.stringResId != R.string.name_dialog_duplicate_name_error_message
@@ -105,19 +109,19 @@ class PlaylistNameValidatorTests {
 
     @Test fun new_name_validator_success() = runTest {
         initNewNameValidator()
-        instance.value = newPlaylistName
+        instance.value = newPresetName
         waitUntil { instance.message != null } // should time out
         val result = instance.validate()
-        assertThat(result).isEqualTo(newPlaylistName)
+        assertThat(result).isEqualTo(newPresetName)
     }
 
     private fun initRenameValidator() {
-        instance = playlistRenameValidator(playlistDao, coroutineScope, existingName1)
+        instance = presetRenameValidator(presetDao, coroutineScope, existingName)
     }
 
     @Test fun rename_validator_begins_with_existing_name_without_error() = runTest {
         initRenameValidator()
-        assertThat(instance.value).isEqualTo(existingName1)
+        assertThat(instance.value).isEqualTo(existingName)
         waitUntil { instance.message != null } // should time out
         assertThat(instance.message).isNull()
     }
@@ -135,7 +139,6 @@ class PlaylistNameValidatorTests {
         initRenameValidator()
         instance.value = ""
         waitUntil { instance.message != null }
-        assertThat(instance.message).isInstanceOf(Validator.Message.Error::class)
         assertThat(instance.message?.stringResource?.stringResId)
             .isEqualTo(R.string.name_dialog_blank_name_error_message)
         assertThat(instance.validate()).isNull()
@@ -143,8 +146,8 @@ class PlaylistNameValidatorTests {
 
     @Test fun rename_validator_success() = runTest {
         initRenameValidator()
-        instance.value = newPlaylistName
+        instance.value = newPresetName
         waitUntil { instance.message != null } // should time out
-        assertThat(instance.validate()).isEqualTo(newPlaylistName)
+        assertThat(instance.validate()).isEqualTo(newPresetName)
     }
 }
