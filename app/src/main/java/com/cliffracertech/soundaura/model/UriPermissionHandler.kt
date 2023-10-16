cea7615575329bbfd8e719ebcf1dd568be0e6ec4
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-10-16 17:17:02 -0400

    Make uri permission handler acquirePermissionsFor return unusable uris instead of usable

--- app/src/main/java/com/cliffracertech/soundaura/model/UriPermissionHandler.kt
+++ app/src/main/java/com/cliffracertech/soundaura/model/UriPermissionHandler.kt
@@ -15,7 +15,8 @@ import javax.inject.Singleton
  * manager of a limited number of persistable file permissions:
  * [acquirePermissionsFor] and [releasePermissionsFor]. */
 interface UriPermissionHandler {
-    val permissionAllowance: Int
+    val totalPermissionAllowance: Int
+    fun remainingPermissionAllowance(): Int
 
     /**
      * Acquire permissions for each file [Uri] in [uris], if space permits. If
@@ -23,11 +24,7 @@ interface UriPermissionHandler {
      * no permissions will be acquired if [allowPartial] is false. Otherwise,
      * as many permissions as space permits will be acquired.
      *
-     * @return The list of uris that the app has permission to use. If the
-     *     remaining permission count is less than the size of [uris] and
-     *     [insertPartial] is true, then the partial list of [Uri]s that the app
-     *     has permissions to use will be returned. If there is not enough space
-     *     and [insertPartial] is false, then an empty list will be returned.
+     * @return The list of uris that the app does NOT have permission to use.
      */
     fun acquirePermissionsFor(uris: List<Uri>, allowPartial: Boolean = true): List<Uri>
 
@@ -37,8 +34,9 @@ interface UriPermissionHandler {
 
 /** A mock [UriPermissionHandler] whose methods do nothing. */
 class TestPermissionHandler(): UriPermissionHandler {
-    override val permissionAllowance = 0
-    override fun acquirePermissionsFor(uris: List<Uri>, allowPartial: Boolean) = uris
+    override val totalPermissionAllowance = Int.MAX_VALUE
+    override fun remainingPermissionAllowance() = Int.MAX_VALUE
+    override fun acquirePermissionsFor(uris: List<Uri>, allowPartial: Boolean): List<Uri> = emptyList()
     override fun releasePermissionsFor(uris: List<Uri>) = Unit
 }
 
@@ -48,23 +46,31 @@ class TestPermissionHandler(): UriPermissionHandler {
  * the list passed to [acquirePermissionsFor], the operation will fail. */
 @Singleton
 class AndroidUriPermissionHandler @Inject constructor(
-    @ApplicationContext private val context: Context,
+    @ApplicationContext context: Context,
 ): UriPermissionHandler {
     private val contentResolver = context.contentResolver
-    override val permissionAllowance =
+
+    override val totalPermissionAllowance =
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) 128
         else                                               512
 
+    override fun remainingPermissionAllowance() =
+        totalPermissionAllowance - contentResolver.persistedUriPermissions.size
+
     override fun acquirePermissionsFor(uris: List<Uri>, allowPartial: Boolean): List<Uri> {
-        val permissionsCount = context.contentResolver.persistedUriPermissions.size
-        val remainingSpace = permissionAllowance - permissionsCount
+        val remainingSpace = remainingPermissionAllowance()
         val hasEnoughSpace = remainingSpace >= uris.size
+        when {
+            hasEnoughSpace -> uris
+            allowPartial ->   uris.subList(0, remainingSpace)
+            else ->           emptyList()
+        }.forEach { contentResolver.takePersistableUriPermission(it, 0) }
 
         return when {
-            hasEnoughSpace ->  uris
-            allowPartial ->    uris.subList(0, remainingSpace)
-            else ->            emptyList()
-        }.onEach { contentResolver.takePersistableUriPermission(it, 0) }
+            hasEnoughSpace -> emptyList()
+            allowPartial ->   uris.subList(remainingSpace, uris.size)
+            else ->           uris
+        }
     }
 
     override fun releasePermissionsFor(uris: List<Uri>) {
