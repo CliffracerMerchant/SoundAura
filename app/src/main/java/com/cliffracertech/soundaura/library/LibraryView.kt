cd518fdd982f118a89ab71c79bcc9c8fb1e9143c
NicholasHochstetler <NicholasHochstetler@gmail.com>
2023-10-16 17:55:01 -0400

    Make library vm use use cases Fix library vm for a71d017 that added file chooser

--- app/src/main/java/com/cliffracertech/soundaura/library/LibraryView.kt
+++ app/src/main/java/com/cliffracertech/soundaura/library/LibraryView.kt
@@ -28,39 +28,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cliffracertech.soundaura.R
 import com.cliffracertech.soundaura.collectAsState
-import com.cliffracertech.soundaura.enumPreferenceFlow
-import com.cliffracertech.soundaura.model.AndroidUriPermissionHandler
 import com.cliffracertech.soundaura.model.MessageHandler
+import com.cliffracertech.soundaura.model.ModifyLibraryUseCase
 import com.cliffracertech.soundaura.model.PlaybackState
 import com.cliffracertech.soundaura.model.PlayerServicePlaybackState
-import com.cliffracertech.soundaura.model.SearchQueryState
-import com.cliffracertech.soundaura.model.StringResource
-import com.cliffracertech.soundaura.model.UriPermissionHandler
-import com.cliffracertech.soundaura.model.database.PlaylistDao
-import com.cliffracertech.soundaura.model.database.playlistRenameValidator
-import com.cliffracertech.soundaura.preferenceFlow
-import com.cliffracertech.soundaura.settings.PrefKeys
+import com.cliffracertech.soundaura.model.ReadLibraryUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-private typealias PlaylistSort = com.cliffracertech.soundaura.model.database.Playlist.Sort
 
 /**
  * A [LazyColumn] to display all of the provided [Playlist]s with instances of [PlaylistView].
@@ -120,125 +102,120 @@ private typealias PlaylistSort = com.cliffracertech.soundaura.model.database.Pla
  * [shownDialog].
  */
 @HiltViewModel class LibraryViewModel(
-    dataStore: DataStore<Preferences>,
-    private val permissionHandler: UriPermissionHandler,
-    private val playlistDao: PlaylistDao,
+    readLibrary: ReadLibraryUseCase,
+    private val modifyLibrary: ModifyLibraryUseCase,
     private val messageHandler: MessageHandler,
     private val playbackState: PlaybackState,
-    searchQuery: SearchQueryState,
     coroutineScope: CoroutineScope? = null
 ) : ViewModel() {
 
     @Inject constructor(
-        dataStore: DataStore<Preferences>,
-        permissionHandler: AndroidUriPermissionHandler,
-        playlistDao: PlaylistDao,
+        readLibraryUseCase: ReadLibraryUseCase,
+        modifyLibraryUseCase: ModifyLibraryUseCase,
         messageHandler: MessageHandler,
         playbackState: PlayerServicePlaybackState,
-        searchQuery: SearchQueryState,
-    ) : this(dataStore, permissionHandler, playlistDao,
-        messageHandler, playbackState, searchQuery, null)
+    ) : this(readLibraryUseCase, modifyLibraryUseCase,
+             messageHandler, playbackState, null)
 
     private val scope = coroutineScope ?: viewModelScope
-    private val showActiveTracksFirstKey = booleanPreferencesKey(PrefKeys.showActivePlaylistsFirst)
-    private val showActiveTracksFirst = dataStore.preferenceFlow(showActiveTracksFirstKey, false)
-    private val playlistSortKey = intPreferencesKey(PrefKeys.playlistSort)
-    private val playlistSort = dataStore.enumPreferenceFlow<PlaylistSort>(playlistSortKey)
 
-    val playlists by combine(
-           playlistSort, showActiveTracksFirst,
-           searchQuery.flow, playlistDao::getAllPlaylists
-        ).transformLatest { emitAll(it) }
-        .map(List<Playlist>::toImmutableList)
-        .collectAsState(null, scope)
+    val playlists by readLibrary.playlistsFlow.collectAsState(null, scope)
 
     var shownDialog by mutableStateOf<PlaylistDialog?>(null)
 
+    private fun dismissDialog() { shownDialog = null }
+
     val itemCallback = object : PlaylistViewCallback {
         override fun onAddRemoveButtonClick(playlist: Playlist) {
-            scope.launch { playlistDao.toggleIsActive(playlist.name) }
+            scope.launch { modifyLibrary.togglePlaylistIsActive(playlist.name) }
         }
         override fun onVolumeChange(playlist: Playlist, volume: Float) {
             playbackState.setPlaylistVolume(playlist.name, volume)
         }
         override fun onVolumeChangeFinished(playlist: Playlist, volume: Float) {
-            scope.launch { playlistDao.setVolume(playlist.name, volume) }
+            scope.launch { modifyLibrary.setPlaylistVolume(playlist.name, volume) }
         }
         override fun onRenameClick(playlist: Playlist) {
             shownDialog = PlaylistDialog.Rename(
                 target = playlist,
-                validator = playlistRenameValidator(playlistDao, scope, playlist.name),
+                validator = modifyLibrary.renameValidator(playlist.name, scope),
                 coroutineScope = scope,
-                onDismissRequest = { shownDialog = null },
+                onDismissRequest = ::dismissDialog,
                 onNameValidated = { validatedName ->
-                    if (validatedName != playlist.name)
-                        playlistDao.rename(playlist.name, validatedName)
-                    shownDialog = null
+                    dismissDialog()
+                    modifyLibrary.renamePlaylist(
+                        from = playlist.name,
+                        to = validatedName)
                 })
         }
         override fun onExtraOptionsClick(playlist: Playlist) {
             scope.launch {
-                val shuffleEnabled = playlistDao.getPlaylistShuffle(playlist.name)
-                val tracks = playlistDao.getPlaylistTracks(playlist.name)
-                shownDialog = PlaylistDialog.PlaylistOptions(
+                val existingTracks = readLibrary.getPlaylistTracks(playlist.name)
+                assert((existingTracks.size == 1) == playlist.isSingleTrack)
+                if (playlist.isSingleTrack)
+                    showFileChooser(playlist, existingTracks)
+                else showPlaylistOptions(
                     target = playlist,
-                    shuffleEnabled = shuffleEnabled,
-                    playlistTracks = tracks,
-                    onDismissRequest = { shownDialog = null },
-                    onConfirm = { newShuffle, newTracks ->
-                        shownDialog = null
-                        savePlaylistShuffleAndTracks(playlist.name, newShuffle, tracks, newTracks)
-                    })
+                    existingTracks = existingTracks,
+                    shuffleEnabled = readLibrary.getPlaylistShuffle(playlist.name))
             }
         }
         override fun onRemoveClick(playlist: Playlist) {
             shownDialog = PlaylistDialog.Remove(
                 target = playlist,
-                onDismissRequest = { shownDialog = null },
+                onDismissRequest = ::dismissDialog,
                 onConfirmClick = {
-                    shownDialog = null
-                    scope.launch {
-                        val unusedTracks = playlistDao.deletePlaylist(playlist.name)
-                        permissionHandler.releasePermissionsFor(unusedTracks)
-                    }
+                    dismissDialog()
+                    scope.launch { modifyLibrary.removePlaylist(playlist.name) }
                 })
         }
     }
 
-    private fun savePlaylistShuffleAndTracks(
-        playlistName: String,
-        shuffle: Boolean,
-        originalTracks: List<Uri>,
-        newTracks: List<Uri>
+    private fun showFileChooser(
+        target: Playlist,
+        existingTracks: List<Uri>,
+        shuffleEnabled: Boolean = false,
     ) {
-        scope.launch {
-            val validatedTrackList =
-                // If no new tracks were added, we can simply save the
-                // new track order. Otherwise, we have to check if there
-                // is enough permission space to add all of the tracks
-                if (newTracks.size == originalTracks.size)
-                    newTracks
-                else permissionHandler.acquirePermissionsFor(
-                    newTracks, allowPartial = false)
-            if (validatedTrackList.isEmpty()) {
-                messageHandler.postMessage(StringResource(
-                    R.string.cant_add_playlist_tracks_warning,
-                    permissionHandler.permissionAllowance))
-                // If there isn't enough space, we ignore the new
-                // track list and only save the new shuffle value
-                playlistDao.setPlaylistShuffle(playlistName, shuffle)
-            } else {
-                val removedUris = playlistDao.setPlaylistShuffleAndContents(
-                    playlistName, shuffle, originalTracks, validatedTrackList)
-                permissionHandler.releasePermissionsFor(removedUris)
+        shownDialog = PlaylistDialog.FileChooser(
+            target, messageHandler, existingTracks,
+            onDismissRequest = {
+                // If the file chooser was arrived at by selecting the 'create playlist'
+                // option for a single track playlist, we want the back button/gesture to
+                // completely dismiss the dialog. If the file chooser was arrived at by
+                // selecting the 'add more files' button in the playlist options dialog
+                // of an existing multi-track playlist, then we want the back button/
+                // gesture to go back to the playlist options dialog for that playlist.
+                if (existingTracks.size == 1)
+                    dismissDialog()
+                else showPlaylistOptions(target, existingTracks, shuffleEnabled)
+            }, onChosenFilesValidated = { validatedFiles ->
+                showPlaylistOptions(target, existingTracks + validatedFiles, shuffleEnabled)
             }
-        }
+        )
+    }
+
+    private fun showPlaylistOptions(
+        target: Playlist,
+        existingTracks: List<Uri>,
+        shuffleEnabled: Boolean,
+    ) {
+        shownDialog = PlaylistDialog.PlaylistOptions(
+            target, existingTracks, shuffleEnabled, ::dismissDialog,
+            onAddFilesClick = {
+                showFileChooser(target, existingTracks, shuffleEnabled)
+            }, onConfirm = { newShuffle, newTrackList ->
+                dismissDialog()
+                scope.launch {
+                    modifyLibrary.setPlaylistShuffleAndTracks(
+                        target.name, newShuffle, newTrackList)
+                }
+            }
+        )
     }
 }
 
 /**
- * Compose a [LibraryView], using an instance of [LibraryViewModel] to
- * obtain the list of tracks and to respond to item related callbacks.
+ * Show a [LibraryView] that uses an instance of [LibraryViewModel] for its state.
  *
  * @param modifier The [Modifier] that will be used for the TrackList.
  * @param padding A [PaddingValues] instance whose values will be
